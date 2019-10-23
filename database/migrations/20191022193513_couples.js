3abe10ed41301f4f62bce7efe260770d61e32c1c
Dan Sample <danssample@gmail.com>
2019-10-22 20:31:59 -0700

    built out first table in database with knex
    Co-authored-by: Brey Batten <bbat104@gmail.com>

--- database/migrations/20191022193513_couples.js
+++ database/migrations/20191022193513_couples.js
@@ -1,8 +1,16 @@
-
 exports.up = function(knex) {
-  
+	return knex.schema.createTable("couples", table => {
+		table.increments();
+		table.string("spouse_one_name").notNullable();
+		table.string("spouse_two_name").notNullable();
+		table
+			.string("email")
+			.notNullable()
+			.unique();
+		table.string("password").notNullable();
+	});
 };
 
 exports.down = function(knex) {
-  
+	return knex.schema.dropTableIfExists("couples");
 };
--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,14 @@
+require("dotenv").config();
 module.exports = {
 	development: {
 		client: "pg",
 		useNullAsDefault: true,
-		connection: "postgres://localhost/wedding_bells_db",
-		pool: {
-			afterCreate: (conn, done) => {
-				conn.run("PRAGMA foreign_keys = ON", done);
-			},
-		},
+		connection: process.env.DB_CONNECTION_URI,
+		// pool: {
+		// 	afterCreate: (conn, done) => {
+		// 		conn.run("PRAGMA foreign_keys = ON", done);
+		// 	},
+		// },
 		migrations: {
 			directory: "./database/migrations",
 		},
