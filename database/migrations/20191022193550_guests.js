87027bb4b3f5c3179482973b7f71efd518c60beb
Dan Sample <danssample@gmail.com>
2019-10-22 20:57:10 -0700

    built out weddings and guests tables in database with knex, also adding .env.example file
    Co-authored-by: Brey Batten <bbat104@gmail.com>

--- database/migrations/20191022193550_guests.js
+++ database/migrations/20191022193550_guests.js
@@ -1,8 +1,24 @@
-
 exports.up = function(knex) {
-  
+	return knex.schema.createTable("guests", table => {
+		table.uuid("id").primary();
+		table.string("name").notNullable();
+		table.string("email").notNullable();
+		table
+			.boolean("is_going")
+			.defaultTo(false)
+			.notNullable();
+		table
+			.boolean("has_responded")
+			.defaultTo(false)
+			.notNullable();
+		table
+			.boolean("plus_one")
+			.defaultTo(true)
+			.notNullable();
+		table.integer("wedding_id").references("weddings.id");
+	});
 };
 
 exports.down = function(knex) {
-  
+	return knex.schema.dropTableIfExists("guests");
 };
