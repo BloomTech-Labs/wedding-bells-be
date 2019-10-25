def750b33de5b12073468fdd427b69f59a392726
mag16 <marcoguzman16@gmail.com>
2019-10-24 21:35:23 -0700

    heroku push

--- knexfile.js
+++ knexfile.js
@@ -16,4 +16,15 @@ module.exports = {
 			directory: "./database/seeds",
 		},
 	},
+	production: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: __dirname + '/db/migrations',
+		},
+		seeds: {
+			directory: __dirname + '/db/seeds/production',
+		},
+	},
 };
+
