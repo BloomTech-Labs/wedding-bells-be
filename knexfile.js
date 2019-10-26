48f72a96968df7ffcb7713681d7bbaef569af19a
mag16 <marcoguzman16@gmail.com>
2019-10-25 19:26:20 -0700

    changed path files

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,8 @@ module.exports = {
 	development: {
 		client: "pg",
 		useNullAsDefault: true,
-		connection: process.env.DB_CONNECTION_URI,
+		connection: process.env.DATABASE_URL,
+		ssl:true,
 		// pool: {
 		// 	afterCreate: (conn, done) => {
 		// 		conn.run("PRAGMA foreign_keys = ON", done);
@@ -20,10 +21,10 @@ module.exports = {
 		client: 'pg',
 		connection: process.env.DATABASE_URL,
 		migrations: {
-			directory: __dirname + '/db/migrations',
+			directory: './database/migrations',
 		},
 		seeds: {
-			directory: __dirname + '/db/seeds/production',
+			directory: './database/seeds/production',
 		},
 	},
 };
