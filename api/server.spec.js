ed4dcbbe1b59790a7a0af305aad34c6df10b129d
mag16 <marcoguzman16@gmail.com>
2019-10-30 19:59:38 -0700

    jest added to dependencies

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const server = require("./api/server")
+const server = require("../api/server")
 
 /* ---------------------- User Endpoints ---------------*/
 describe('/api/users CRUD', function() {
