ba797cbbea87966a79b9e1d3846abdbb86955377
Cedricium <amaya.cedric@gmail.com>
2019-10-30 19:37:17 -0700

    Skeleton code for the Guests router and model

--- api/server.js
+++ api/server.js
@@ -5,13 +5,11 @@ const helmet = require("helmet");
 const session = require("express-session");
 const knexSessionStore = require("connect-session-knex")(session);
 const server = express();
+
 const usersRouter = require("../routes/userRouter");
 const vendorsRouter = require("../routes/vendorRouter");
 const weddingsRouter = require("../routes/weddingRouter");
-
-//Import routes from the router file here
-// const example1Router = require("../routers/example1Router");
-// const example2Router = require("../routers/example2Router");
+const guestsRouter = require("../routes/guestsRouter");
 
 //Import the secrets file for jsonwebtoken here
 const secrets = require("../config/secrets.js");
@@ -50,11 +48,7 @@ server.use(session(sessionOptions));
 server.use("/api/users", usersRouter);
 server.use("/api/vendors", vendorsRouter);
 server.use("/api/weddings", weddingsRouter);
-//Insert routes for routers here
-//This one does not use authentication middleware
-// server.use("/api/ex1", example1Router);
-// //This one does use authentication middleware
-// server.use("/api/ex2", restricted, example2Router);
+server.use("/api/guests", guestsRouter);
 
 //This is what is shown from the backend when you go to the localhost:5000/
 server.get("/", (req, res) => {
---
+++ models/guests.js
@@ -0,0 +1,17 @@
+const db = require("../database/config");
+
+const find = async () => {};
+const findById = async id => {};
+const findByFilter = async filter => {};
+const add = async guest => {};
+const update = async (id, guest) => {};
+const remove = async id => {};
+
+module.exports = {
+	find,
+	findById,
+	findByFilter,
+	add,
+	update,
+	remove,
+};
