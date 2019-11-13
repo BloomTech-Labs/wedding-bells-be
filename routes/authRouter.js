64c3231300728b74ffe34c13f03cdd4c9795b06b
schang1146 <sammychang2185@gmail.com>
2019-11-12 19:10:45 -0800

    added register POST

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -22,6 +22,32 @@ function generateToken(user) {
 }
 
 // post user w/ hashed password to register
+router.post("/register", async (req, res) => {
+	const { spouse_one_name, spouse_two_name, email, password } = req.body;
+	// catch if empty field
+	if (!spouse_one_name || !spouse_two_name || !email || !password) {
+		return res.status(400).json({
+			error: "`spouse_one_name`, `spouse_two_name`, `email`, and `password` are required!"
+		});
+	}
+	// insert user with hashed password
+	try {
+		const hash = bcrypt.hashSync(password, 10);
+		const [id] = await db("couples").insert({
+			spouse_one_name,
+			spouse_two_name,
+			email,
+			password: hash,
+		});
+		const [couple] = await db("couples").where({ id });
+		return res.status(201).json(couple);
+	// error
+	} catch (error) {
+		res.status(500).json({
+			error: error.message
+		});
+	}
+});
 
 // post existing user so they can login
 
