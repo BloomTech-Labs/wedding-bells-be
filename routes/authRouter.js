0ddb0c3e132e07b33de5d43429c1f74fd18a365c
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-12-04 22:19:26 -0500

    took out useDefaultasNull from knexfile to solve role issue. Added token to registered user

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -50,8 +50,24 @@ router.post("/register", async (req, res) => {
 			.returning("id");
 
 		const [couple] = await db("couples").where({ id });
+		if (couple && bcrypt.compareSync(password, couple.password)) {
+			const token = generateToken(couple);
 
-		return res.status(201).json(couple);
+			await db("couples")
+				.where({ email })
+				.update({ jwt: token });
+
+			return res.status(201).json({
+				message: `Welcome ${couple.spouse_one_name} and ${couple.spouse_two_name}`,
+				token: token,
+				couple,
+			});
+		} else {
+			return res.status(401).json({
+				error:
+					"You're killing me smalls! You need to provide matching and existing credentials",
+			});
+		}
 		// error
 	} catch (error) {
 		res.status(500).json({
